d00a68d6f54c9b34a1bf215213ee4c4ac612997c
academicsociety2 <elembe2222@gmail.com>
2026-09-09 17:51:05 +0300

    Add files via upload

---
+++ Data/محمد اللحيدان/الزلزلة/الزلزلة.js
@@ -0,0 +1,12 @@
+// Data/محمد اللحيدان/الزلزلة/الزلزلة.js
+window.currentSurahVerses = [
+    { id: 0, start_time: 0.0, end_time: 3.0, text: "بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ", type: "bismillah" },
+    { id: 1, start_time: 3.0, end_time: 7.0, text: "إِذَا زُلۡزِلَتِ ٱلۡأَرۡضُ زِلۡزَالَهَا", type: "aya" },
+    { id: 2, start_time: 7.0, end_time: 11.0, text: "وَأَخۡرَجَتِ ٱلۡأَرۡضُ أَثۡقَالَهَا", type: "aya" },
+    { id: 3, start_time: 11.0, end_time: 15.0, text: "وَقَالَ ٱلۡإِنسَـٰنُ مَا لَهَا", type: "aya" },
+    { id: 4, start_time: 15.0, end_time: 19.0, text: "یَوۡمَىِٕذࣲ تُحَدِّثُ أَخۡبَارَهَا", type: "aya" },
+    { id: 5, start_time: 19.0, end_time: 23.0, text: "بِأَنَّ رَبَّكَ أَوۡحَىٰ لَهَا", type: "aya" },
+    { id: 6, start_time: 23.0, end_time: 30.0, text: "یَوۡمَىِٕذࣲ یَصۡدُرُ ٱلنَّاسُ أَشۡتَاتࣰا لِّیُرَوۡا۟ أَعۡمَـٰلَهُمۡ", type: "aya" },
+    { id: 7, start_time: 30.0, end_time: 36.0, text: "فَمَن یَعۡمَلۡ مِثۡقَالَ ذَرَّةٍ خَیۡرࣰا یَرَهُۥ", type: "aya" },
+    { id: 8, start_time: 36.0, end_time: 43.0, text: "وَمَن یَعۡمَلۡ مِثۡقَالَ ذَرَّةࣲ شَرࣰّا یَرَهُۥ", type: "aya" }
+];
---
+++ Data/محمد اللحيدان/العاديات/العاديات.js
@@ -0,0 +1,15 @@
+// Data/محمد اللحيدان/العاديات/العاديات.js
+window.currentSurahVerses = [
+    { id: 0, start_time: 0.0, end_time: 3.0, text: "بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ", type: "bismillah" },
+    { id: 1, start_time: 3.0, end_time: 6.0, text: " وَٱلۡعَـٰدِیَـٰتِ ضَبۡحࣰا", type: "aya" },
+    { id: 2, start_time: 6.0, end_time: 9.0, text: "فَٱلۡمُورِیَـٰتِ قَدۡحࣰا", type: "aya" },
+    { id: 3, start_time: 9.0, end_time: 11.0, text: "فَٱلۡمُغِیرَ ٰ⁠تِ صُبۡحࣰا", type: "aya" },
+    { id: 4, start_time: 11.0, end_time: 14.0, text: "فَأَثَرۡنَ بِهِۦ نَقۡعࣰا", type: "aya" },
+    { id: 5, start_time: 14.0, end_time: 16.0, text: "فَوَسَطۡنَ بِهِۦ جَمۡعًا", type: "aya" },
+    { id: 6, start_time: 16.0, end_time: 22.0, text: "إِنَّ ٱلۡإِنسَـٰنَ لِرَبِّهِۦ لَكَنُودࣱ", type: "aya" },
+    { id: 7, start_time: 22.0, end_time: 27.0, text: "وَإِنَّهُۥ عَلَىٰ ذَ ٰ⁠لِكَ لَشَهِیدࣱ", type: "aya" },
+    { id: 8, start_time: 27.0, end_time: 31.0, text: "وَإِنَّهُۥ لِحُبِّ ٱلۡخَیۡرِ لَشَدِیدٌ", type: "aya" },
+    { id: 9, start_time: 31.0, end_time: 36.0, text: "۞ أَفَلَا یَعۡلَمُ إِذَا بُعۡثِرَ مَا فِی ٱلۡقُبُورِ", type: "aya" },
+    { id: 10, start_time: 36.0, end_time: 40.0, text: "وَحُصِّلَ مَا فِی ٱلصُّدُورِ", type: "aya" },
+    { id: 11, start_time: 40.0, end_time: 48.0, text: "إِنَّ رَبَّهُم بِهِمۡ یَوۡمَىِٕذࣲ لَّخَبِیرُۢ", type: "aya" }
+];
